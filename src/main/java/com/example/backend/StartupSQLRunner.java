234bc7b0f099e922da19932a3170885c1a45795a
Aidan <aizimmerman03@gmail.com>
2025-04-19 02:09:15 -0700

    Fixed test sql insertion

--- src/main/java/com/example/backend/StartupSQLRunner.java
+++ src/main/java/com/example/backend/StartupSQLRunner.java
@@ -25,7 +25,17 @@ public class StartupSQLRunner {
         jdbcTemplate.execute("""
                              insert into user (id,name,age,gender,description,height,race,religion,instagram,spotify,leetcode,picture)
                              values (42,'Ella Jones',22,'Female','Personally, I love UI/UX, but I also picked up an interest in backends ;)',160,null,null,null,null,null,null);""");
-
+        jdbcTemplate.execute("""
+                             INSERT INTO user (id, name, age, gender, description, height, race, religion, instagram, spotify, leetcode, picture)
+                             VALUES (36, 'Isla Anderson', 19, 'Female', 'I\\'m studying Gaming and would love to collaborate on anything related to AI.', 156, 'Mixed', 'Spiritual', NULL, 'https://spotify.com/user/isla979', NULL, 'https://randomuser.me/api/portraits/women/44.jpg');""");
+        jdbcTemplate.execute("""
+                             INSERT INTO user_interests (user_id, interests) VALUES (36, 'Gaming'), (36, 'Hackathons'), (36, 'Competitive Programming'), (36, 'Friends');""");
+        jdbcTemplate.execute("""
+                             INSERT INTO user (id, name, age, gender, description, height, race, religion, instagram, spotify, leetcode, picture)
+                             VALUES (37, 'Alice Miller', 23, 'Female', 'I\\'m currently studying Gaming and love diving deep into Cybersecurity projects.', 155, 'Other', NULL, NULL, 'https://spotify.com/user/alice816', NULL, 'https://randomuser.me/api/portraits/women/19.jpg');""");
+        jdbcTemplate.execute("""
+        INSERT INTO user_interests (user_id, interests) VALUES (37, 'UI/UX'), 
+                             (37, 'Mobile Apps'),(37, 'Dating'),(37, 'Friends');""");
     }
 
 }
