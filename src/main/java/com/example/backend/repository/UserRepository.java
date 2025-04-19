ab6acf31b3d8b7fefb5fa0e1368eb4cba9e87601
Aidan <aizimmerman03@gmail.com>
2025-04-19 02:15:56 -0700

    Readded findAll method

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.backend.repository;
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-// import java.util.*;
+import java.util.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
@@ -11,5 +11,5 @@ public interface UserRepository extends JpaRepository<User, String> {
     // Custom query to find a user by email
     User getUserById(Long id);
 
-    // List<User> getAllUsers();
+    List<User> findAll();
 }
