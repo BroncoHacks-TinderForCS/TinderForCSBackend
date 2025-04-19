420bbc1f9d155be449675bcbb289134a9f5e0d8a
Aidan <aizimmerman03@gmail.com>
2025-04-18 21:40:23 -0700

    Fixed method in functional interface UserRepository, added creation of functional interace UserRepository in MatchService

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,12 +3,12 @@ package com.example.backend.repository;
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     // Custom query to find a user by email
     User getUserByID(String ID);
-    @Override
-    String toString();
+    List<User> getAllUsers();
 }
