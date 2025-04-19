aa4952d44c70fd5bad97be42d915205ab8017729
Maayan Israel <maayan@b01-aruba-authenticated-10-110-198-154.wlan.cpp.edu>
2025-04-19 15:21:44 -0700

    asd

--- src/main/java/com/example/backend/WebConfig.java
+++ src/main/java/com/example/backend/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                        .allowedOrigins("http://localhost:5173") // Allow requests from this origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies or credentials
