13943a741365be73119bc81f97c052895f8855c3
Aidan <aizimmerman03@gmail.com>
2025-04-19 01:16:36 -0700

    Added working sql entries

--- src/main/java/com/example/backend/StartupSQLRunner.java
+++ src/main/java/com/example/backend/StartupSQLRunner.java
@@ -17,17 +17,14 @@ public class StartupSQLRunner {
     @EventListener(ApplicationReadyEvent.class)
     public void runSQLQueries() {
         // Insert 5 users
-        /*
-        jdbcTemplate.execute(
-                "INSERT INTO users (id, name, email) VALUES ('1', 'John Doe', 'john.doe@example.com')");
-        jdbcTemplate.execute(
-                "INSERT INTO users (id, name, email) VALUES ('2', 'Jane Smith', 'jane.smith@example.com')");
-        jdbcTemplate.execute(
-                "INSERT INTO users (id, name, email) VALUES ('3', 'Robert Brown', 'robert.brown@example.com')");
-        jdbcTemplate.execute(
-                "INSERT INTO users (id, name, email) VALUES ('4', 'Emily Davis', 'emily.davis@example.com')");
-        jdbcTemplate.execute(
-                "INSERT INTO users (id, name, email) VALUES ('5', 'Michael Johnson', 'michael.johnson@example.com')");*/
+        
+        jdbcTemplate.execute("""
+                             INSERT INTO user (id, name, age, gender, description, height, race, religion, instagram, spotify, leetcode, picture)
+                             VALUES (1, 'Daniel Miller', 25, 'Male', 'Hackathons has been my main interest, but recently I''ve started enjoying Cybersecurity too.', 170, NULL, 'Muslim', 'https://instagram.com/daniel89', NULL, NULL, 'https://randomuser.me/api/portraits/men/44.jpg');""");
+        jdbcTemplate.execute("INSERT INTO user_interests (user_id, interests) VALUES (1, 'Embedded Systems');");
+        jdbcTemplate.execute("""
+                             insert into user (id,name,age,gender,description,height,race,religion,instagram,spotify,leetcode,picture)
+                             values (42,'Ella Jones',22,'Female','Personally, I love UI/UX, but I also picked up an interest in backends ;)',160,null,null,null,null,null,null);""");
 
     }
 
